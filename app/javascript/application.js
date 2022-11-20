9de694303f9e3ede78556dac507413a21212a2f6
arnaud rigourd <arnaud.rigourd@hotmail.fr>
2022-11-20 22:54:24 +0100

    add some style, but not finished yet

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,7 @@
 // Entry point for the build script in your package.json
+import Swup from 'swup';
 import "@hotwired/turbo-rails"
 import "./controllers"
 import 'bootstrap'
+
+const swup = new Swup();
